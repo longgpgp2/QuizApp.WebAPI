650a506d10533597d185f172adc8506f3ef567c8
long <longnguyenthanh2003@gmail.com>
2025-03-09 17:06:07 +0700

    Update gitignore

--- QuizApp.Data/obj/Debug/net9.0/QuizApp.Data.AssemblyInfo.cs
+++ QuizApp.Data/obj/Debug/net9.0/QuizApp.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizApp.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7516c455a3f9ddd94e6d3af520e9d4481e7c448")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuizApp.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuizApp.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- QuizApp.Models/obj/Debug/net9.0/QuizApp.Models.AssemblyInfo.cs
+++ QuizApp.Models/obj/Debug/net9.0/QuizApp.Models.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizApp.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7516c455a3f9ddd94e6d3af520e9d4481e7c448")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuizApp.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuizApp.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
